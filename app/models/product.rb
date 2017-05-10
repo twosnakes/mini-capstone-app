d589626395151b73d5d50f8ff1f982104a3eff6d
Zoe Lembeck <zlembeck@gmail.com>
2017-05-09 21:42:52 -0500

    new models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+
+  def friendly_updated_at
+    updated_at.strftime("%m/%d/%Y")
+  end
+
+  def name_and_description
+    "{name} #{description}"
+  end
+
+  def sale_message
+    if price < $250
+      "" 
+    else "Regularly Priced"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'products#index'
   get '/products' => 'products#index'
   get '/products/new'=> 'products#new'
-  
   get '/products/:id' => 'products#show'
   post '/products' => 'products#create'
   get '/products/:id/edit' => 'products#edit'
