a99f940e6393f56f2b7e147938f70ff2d3e808e8
Zoe Lembeck <zlembeck@gmail.com>
2017-05-16 21:51:20 -0500

    added new model called imagese

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,15 @@
 class Product < ApplicationRecord
   belongs_to :supplier
+  has_many :images
+
+  def first_image_url
+    image_collection = images
+    if image_collection.length == 0
+      "http://2.bp.blogspot.com/-jtgPyWdc7mA/WPMTJK64BVI/AAAAAAAAGKc/35X90QYVgYUmeTYI0CFCvwjMbEYCnsZYQCK4B/s1600/Onions%2B%2526%2BGarlic.jpg"
+    else 
+      images.first.url
+    end
+  end
 
   def friendly_updated_at
     updated_at.strftime("%m/%d/%Y")
