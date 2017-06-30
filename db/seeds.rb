855f3f2eb2ec5e330364a63b8d38546564b1f011
Zoe Lembeck <zlembeck@gmail.com>
2017-06-29 20:50:31 -0500

    uncommented seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,30 +3,30 @@
 #
 # Examples:
 #
-  # product = Product.new(name: "Epic", price: 300, image: "http://2.bp.blogspot.com/-m6IV1Pp4xCw/WPMRmPqI08I/AAAAAAAAGKA/kyF7pG6r6nkEGDaZhJDPtlbUgsA3IqVrACK4B/s1600/Epic.jpg", description: "Black and white photo of two girls in Havana, Cuba")
-  # product.save
-  # product = Product.new(name: "La Habana", price: 300, image: "http://3.bp.blogspot.com/-13P1t4UYUjA/WPZiWg3AsUI/AAAAAAAAGME/pZJ9KLsUHecm36ifpvZsWwgoPnBCZCkugCK4B/s1600/La%2BHabana.jpg", description: "Black and white street in Havana, Cuba")
-  # product.save
-  # product = Product.new(name: "Collecting Garbage", price: 300, image: "http://2.bp.blogspot.com/-AVIlyO_dRuE/WPMFjg6SnMI/AAAAAAAAGIU/5PE-OQNwXskOXp-EhAZGFKYJ0b3ukKi3gCK4B/s1600/Ras.jpg", description: "Black and white photo of men collecting garbage in Havana, Cuba")
-  # product.save
-  # product = Product.new(name: "Cousins", price: 300, image: "http://1.bp.blogspot.com/-AOErcmRKKKg/WPMD50fgp3I/AAAAAAAAGHs/QHlvsOWbvQgsPt1qPQnfHnPbgywPS_wuQCK4B/s1600/Fishing%2BBoys.jpg", description: "Black and white photo of cousins in Havana, Cuba")
-  # product.save
-  # product = Product.new(name: "Arcs", price: 300, image: "http://4.bp.blogspot.com/-l49E3dG9qno/WPMSuQhxvfI/AAAAAAAAGKU/oZW0Vij292EYrubV-QnPlbILzlihSLMAgCK4B/s1600/Arcs.jpg", description: "Black and white photo of arcs in Havana, Cuba")
-  # product.save
-  # product = Product.new(name: "Husky In The Window", price: 300, image: "http://2.bp.blogspot.com/-MTpdNfF7v-4/WPMEmKcgOyI/AAAAAAAAGIA/AhGxAvstzw80tinK5a6q_RBa9dtyfaDBQCK4B/s1600/Husky%2BIn%2BThe%2BWindow.jpg", description: "Black and white photo of Husky in Havana, Cuba")
-  # product.save
-  #  product = Product.new(name: "Sass", price: 300, image: "http://4.bp.blogspot.com/-KBGlML58too/WPMGZekRdgI/AAAAAAAAGIk/zp71ZRMTxrAGJG9v0aNzjYWAk1YJaWrIQCK4B/s1600/Sass.jpg", description: "Black and white photo of women walking in Havana, Cuba")
-  # product.save
-   # product = Product.new(name: "Rollerbladers", price: 300, description: "Black and white photo of rollerbladers in Havana, Cuba", supplier_id: 3)
-   # product.save
-  # product = Product.new(name: "After School Girl", price: 300, image: "http://1.bp.blogspot.com/-Ix_NFvs4n5Q/WPMGuOwTuLI/AAAAAAAAGIs/uoJxJLyqjTsvslvT0rX7FgedfQkoC25lwCK4B/s1600/Schoolgirl.jpg", description: "Black and white photo of girl after school in Havana, Cuba")
-  # product.save
+   product = Product.new(name: "Epic", price: 300, image: "http://2.bp.blogspot.com/-m6IV1Pp4xCw/WPMRmPqI08I/AAAAAAAAGKA/kyF7pG6r6nkEGDaZhJDPtlbUgsA3IqVrACK4B/s1600/Epic.jpg", description: "Black and white photo of two girls in Havana, Cuba")
+   product.save
+   product = Product.new(name: "La Habana", price: 300, image: "http://3.bp.blogspot.com/-13P1t4UYUjA/WPZiWg3AsUI/AAAAAAAAGME/pZJ9KLsUHecm36ifpvZsWwgoPnBCZCkugCK4B/s1600/La%2BHabana.jpg", description: "Black and white street in Havana, Cuba")
+   product.save
+   product = Product.new(name: "Collecting Garbage", price: 300, image: "http://2.bp.blogspot.com/-AVIlyO_dRuE/WPMFjg6SnMI/AAAAAAAAGIU/5PE-OQNwXskOXp-EhAZGFKYJ0b3ukKi3gCK4B/s1600/Ras.jpg", description: "Black and white photo of men collecting garbage in Havana, Cuba")
+   product.save
+   product = Product.new(name: "Cousins", price: 300, image: "http://1.bp.blogspot.com/-AOErcmRKKKg/WPMD50fgp3I/AAAAAAAAGHs/QHlvsOWbvQgsPt1qPQnfHnPbgywPS_wuQCK4B/s1600/Fishing%2BBoys.jpg", description: "Black and white photo of cousins in Havana, Cuba")
+   product.save
+   product = Product.new(name: "Arcs", price: 300, image: "http://4.bp.blogspot.com/-l49E3dG9qno/WPMSuQhxvfI/AAAAAAAAGKU/oZW0Vij292EYrubV-QnPlbILzlihSLMAgCK4B/s1600/Arcs.jpg", description: "Black and white photo of arcs in Havana, Cuba")
+  product.save
+   product = Product.new(name: "Husky In The Window", price: 300, image: "http://2.bp.blogspot.com/-MTpdNfF7v-4/WPMEmKcgOyI/AAAAAAAAGIA/AhGxAvstzw80tinK5a6q_RBa9dtyfaDBQCK4B/s1600/Husky%2BIn%2BThe%2BWindow.jpg", description: "Black and white photo of Husky in Havana, Cuba")
+   product.save
+   product = Product.new(name: "Sass", price: 300, image: "http://4.bp.blogspot.com/-KBGlML58too/WPMGZekRdgI/AAAAAAAAGIk/zp71ZRMTxrAGJG9v0aNzjYWAk1YJaWrIQCK4B/s1600/Sass.jpg", description: "Black and white photo of women walking in Havana, Cuba")
+   product.save
+   product = Product.new(name: "Rollerbladers", price: 300, description: "Black and white photo of rollerbladers in Havana, Cuba", supplier_id: 3)
+   product.save
+   product = Product.new(name: "After School Girl", price: 300, image: "http://1.bp.blogspot.com/-Ix_NFvs4n5Q/WPMGuOwTuLI/AAAAAAAAGIs/uoJxJLyqjTsvslvT0rX7FgedfQkoC25lwCK4B/s1600/Schoolgirl.jpg", description: "Black and white photo of girl after school in Havana, Cuba")
+   product.save
 
-   # Image.create!(url: "http://3.bp.blogspot.com/-13P1t4UYUjA/WPZiWg3AsUI/AAAAAAAAGME/pZJ9KLsUHecm36ifpvZsWwgoPnBCZCkugCK4B/s1600/La%2BHabana.jpg", product_id: 2 )
- #
+    Image.create!(url: "http://3.bp.blogspot.com/-13P1t4UYUjA/WPZiWg3AsUI/AAAAAAAAGME/pZJ9KLsUHecm36ifpvZsWwgoPnBCZCkugCK4B/s1600/La%2BHabana.jpg", product_id: 2 )
+ 
 
-  # Image.create!(url: "http://4.bp.blogspot.com/-skbByjZTk-I/WPMF73m_rLI/AAAAAAAAGIc/41SwI3Sq_4oEJcDbs5NepMbhzt_Zuc-6wCK4B/s1600/Rollerbladers.jpg", product_id: 12)
- #
+   Image.create!(url: "http://4.bp.blogspot.com/-skbByjZTk-I/WPMF73m_rLI/AAAAAAAAGIc/41SwI3Sq_4oEJcDbs5NepMbhzt_Zuc-6wCK4B/s1600/Rollerbladers.jpg", product_id: 12)
+ 
 
  #4 - expensive
  #5 - black and white
